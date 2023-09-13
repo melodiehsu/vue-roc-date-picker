66342fd2432793cdafc5c3f483b2108274543db3
Melody <melodyuf1b@gmail.com>
2023-09-13 14:26:04 +0800

    fix: added test coverage and added test case to utils

--- src/utils/__tests__/set-date-picker-label.test.ts
+++ src/utils/__tests__/set-date-picker-label.test.ts
@@ -3,7 +3,7 @@ import { CALENDAR_TYPE, YEAR_TYPE } from '@/constants';
 import { setDatePickerLabel } from '../index';
 
 describe('setDatePickerLabel', () => {
-  it('should return Republic Era Year', () => {
+  it('should return Republic Era Year to date', () => {
     const result = setDatePickerLabel({
       calendarYearType: YEAR_TYPE.RepublicEraYear,
       selectedDateObject: new Date('2023/09/11'),
@@ -12,5 +12,27 @@ describe('setDatePickerLabel', () => {
     });
 
     expect(result).toBe('112/09/11');
+  });
+
+  it('should return Republic Era Year to year', () => {
+    const result = setDatePickerLabel({
+      calendarYearType: YEAR_TYPE.RepublicEraYear,
+      selectedDateObject: new Date('2023/09/11'),
+      formatYear: 2023,
+      datePickerType: CALENDAR_TYPE.year
+    });
+
+    expect(result).toBe('112');
+  });
+
+  it('should return Common Era Year to date', () => {
+    const result = setDatePickerLabel({
+      calendarYearType: YEAR_TYPE.CE,
+      selectedDateObject: new Date('2023/09/11'),
+      formatYear: 2023,
+      datePickerType: CALENDAR_TYPE.date
+    });
+
+    expect(result).toBe('2023-09-11');
   });
 });
