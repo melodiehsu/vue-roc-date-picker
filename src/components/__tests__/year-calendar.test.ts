afe264ffc72801b1aeaf00f838c462f51d7625b3
Melody <melodyuf1b@gmail.com>
2023-09-19 14:49:15 +0800

    chore: remove redundant code

--- src/components/__tests__/year-calendar.test.ts
+++ src/components/__tests__/year-calendar.test.ts
@@ -112,13 +112,3 @@ describe('Test Component YearCalendar', () => {
     expect(yearValue).toBe(`${wrapper.vm.years[randomIndex]}`);
   });
 });
-
-describe('Test getRepublicEraYear', () => {
-  it('pass 2023 returns 112', () => {
-    expect(getRepublicEraYear(2023)).toBe(112);
-  });
-
-  it('pass 1900 returns -11', () => {
-    expect(getRepublicEraYear(1900)).toBe(-11);
-  });
-});
