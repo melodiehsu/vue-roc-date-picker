427670bef1bb8c201f43b07ddf51e2e68163f7c0
Melody <melodyuf1b@gmail.com>
2023-09-30 08:35:41 +0800

    fix: update index.d.ts

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,25 +1,27 @@
-import { Plugin } from 'vue';
+declare module 'vue3-roc-date-picker' {
+  import { Plugin } from 'vue';
 
-export interface SelectedTime {
-  label?: string;
-  timeValue?: Date | undefined;
+  export interface SelectedTime {
+    label?: string;
+    timeValue?: Date | undefined;
+  }
+
+  export enum Language {
+    EN = 'en',
+    ZH_TW = 'zhTW'
+  }
+
+  export enum YearType {
+    CommonEra = 'CommonEra',
+    RepublicEra = 'RepublicEra'
+  }
+
+  export enum CalendarType {
+    YEAR = 'year',
+    MONTH = 'month',
+    DATE = 'date'
+  }
+
+  const ROCDatePicker: Plugin;
+  export default ROCDatePicker;
 }
-
-export enum Language {
-  EN = 'en',
-  ZH_TW = 'zhTW'
-}
-
-export enum YearType {
-  CommonEra = 'CommonEra',
-  RepublicEra = 'RepublicEra'
-}
-
-export enum CalendarType {
-  YEAR = 'year',
-  MONTH = 'month',
-  DATE = 'date'
-}
-
-const ROCDatePicker: Plugin;
-export default ROCDatePicker;
